7f29969180ee9647a1bac2deeee4329f4f4c6241
karlkastner <kastner.karl@gmail.com>
2019-10-08 10:13:58 +0800

    Improved documentation and added scripts to recursively fetch the source code

---
+++ load_svn_externals.m
@@ -0,0 +1,28 @@
+% Tue  8 Oct 09:33:30 PST 2019
+% Karl Kastner, Berlin
+%
+%% script emulating svn:external, which is not supported by GitHub
+%
+file_str = 'svn-externals.csv';
+tab = readtable(file_str,'HeaderLines',0,'Delimiter','tab','MultipleDelimsAsOne',1);
+for idx=1:size(tab,1)
+	dir_str  = tab{idx,1}{1};
+	rev      = tab{idx,2}(1);
+	rev_str  = num2str(rev);
+	repo_str = tab{idx,3}{1};
+	% trailing slash necessary, workaround for matlab bug detecting non-existing directories
+	if (~exist([dir_str,'/'],'dir'))
+		[stat, ret_str] = system(['svn info --show-item revision ',repo_str]);
+		rev_latest = str2num(ret_str);
+		if (rev_latest > rev)
+			printf(['Note: Newer revision available for repository ',repo_str,'\n']);
+		end
+		%if (~exist(dir_str,'dir')
+			% check out
+			system(['svn checkout -r',rev_str,' ',repo_str,' ',dir_str]);
+		%end
+	else
+		printf(['Directory ',dir_str,' exists already, nothing to do\n']);
+	end % else of if ~exist
+end % for idx
+ 
---
+++ startup.m
@@ -0,0 +1,9 @@
+% Tue  8 Oct 09:47:41 PST 2019
+% Karl Kastner, Berlin
+
+% fetch sub-repositories
+load_svn_externals();
+
+% add sub-repositories to search path
+addpath_recursive(['src/lib']);
+
